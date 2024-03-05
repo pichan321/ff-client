df1c2393d512a959154faa1399201f6e17d355b2
pichan321 <pchan@ithaca.edu>
2024-03-05 17:46:38 -0500

    initiated tailwind css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='h-[150px] b bg-slate-500'>
+        <p className='font font-bold'>Hello</p>
+      </div>
     </div>
   );
 }
