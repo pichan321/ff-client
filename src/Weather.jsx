ecc7fa3f339d5037ecb5b5a4ef811a6d1b66bb18
jguidi1 <jguidi@ithaca.edu>
2024-03-25 18:07:28 -0400

    update push

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,29 +1,39 @@
 import { useEffect, useState } from "react"
 import { Button, Input, TextInput } from '@mantine/core';
 import axios from "axios";
+import useFetch from "./useFetch";
 //https://api.weatherapi.com/v1/current.json?key=7d80e451ec6a4c2fb6e35425220203&q=London&aqi=no
 
 export default function Weather() {
     const [city, setCity] = useState('')
     const [allCities, setAllCities] = useState([])
+    const fetcher = useFetch()
 
 
     const addNewCity = async () => {
+        // try {
+        //     var response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=7d80e451ec6a4c2fb6e35425220203&q=${city}&aqi=no`)
+        //     var data = response.data
+        //     setAllCities([...allCities, {city: data.location.name, temp_c: data.current.temp_c, temp_f: data.current.temp_f}])
+        // } catch {
+        //     return
+        // }
+        
+        var data = await fetcher.GET(`https://api.weatherapi.com/v1/current.json?key=7d80e451ec6a4c2fb6e35425220203&q=${city}&aqi=no`)
 
-        try {
-            var response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=7d80e451ec6a4c2fb6e35425220203&q=${city}&aqi=no`)
-            var data = response.data
-            setAllCities([...allCities, {city: data.location.name, temp_c: data.current.temp_c, temp_f: data.current.temp_f}])
-        } catch {
-            return
+        if (!data) {
+            console.log("failed to fetch")
         }
-    
+
+        console.log("successful fetch")
     
     }
 
     return (
         <div>
-
+            
+                <h5>{JSON.stringify(fetcher.loading)}</h5>
+            
             <div className="grid grid-cols-12">
                 <div className="col-span-12">
                     <div className="flex justify-center items-center">
